cb60c928c61a29028b46a9789deeaa4e9169cd55
Gursaanj <gursaanjresides@gmail.com>
2021-02-24 00:34:40 -0800

    Starting Objects

---
+++ FNF/Assets/Frisbee Mechanics/Scripts/FrisbeeLauncher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FrisbeeLauncher : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _frisbee;
+    [SerializeField] private Transform _target;
+    [SerializeField, Range(0, 100)] private float _initialSpeed;
+
+    [SerializeField] private KeyCode _launchButton = KeyCode.Space;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_launchButton))
+        {
+            Launch();
+        }
+    }
+
+    private void Launch()
+    {
+        if (_frisbee == null)
+        {
+            return;
+        }
+
+        _frisbee.transform.parent = null;
+        _frisbee.useGravity = false;
+        _frisbee.velocity = Vector3.forward * _initialSpeed;
+    }
+}
---
+++ FNF/Assets/Frisbee Mechanics/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Player : MonoBehaviour
+{
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     other.collider.isTrigger = false;
+    // }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.isTrigger = false;
+    }
+}
