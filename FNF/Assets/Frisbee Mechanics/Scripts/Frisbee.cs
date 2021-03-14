d1087f269df33f91a4ae97f45453440e5cb5320f
Gursaanj <gursaanjresides@gmail.com>
2021-03-14 13:44:02 -0400

    Updated Methodology

---
+++ FNF/Assets/Frisbee Mechanics/Scripts/Frisbee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Frisbee : MonoBehaviour
+{
+    public Action InAction;
+
+    private void FixedUpdate()
+    {
+        if (InAction != null)
+        {
+            
+        }
+    }
+}
--- FNF/Assets/Frisbee Mechanics/Scripts/FrisbeeLauncher.cs
+++ FNF/Assets/Frisbee Mechanics/Scripts/FrisbeeLauncher.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FrisbeeLauncher : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _frisbee;
-    [SerializeField] private Transform _target;
+    [SerializeField] private Frisbee _frisbee;
+    [SerializeField] private Transform _curvePoint;
     [SerializeField, Range(0, 100)] private float _initialSpeed;
     
     [Header("Debug Controls")]
@@ -15,9 +14,12 @@ public class FrisbeeLauncher : MonoBehaviour
 
 
     private Transform _parent;
+    private Rigidbody _frisbeeRb;
     private bool _hasLaunched = false;
+    
     private void Start()
     {
+        _frisbeeRb = _frisbee.GetComponent<Rigidbody>();
         _parent = _frisbee.transform.parent;
     }
 
@@ -41,39 +43,31 @@ public class FrisbeeLauncher : MonoBehaviour
         {
             return;
         }
-
-        _frisbee.transform.parent = null;
-        _frisbee.useGravity = false;
+        
+        _frisbeeRb.transform.parent = null;
+        _frisbeeRb.useGravity = false;
         //_frisbee.velocity = Vector3.forward * _initialSpeed;
-        _frisbee.AddForce(transform.forward*_initialSpeed, _forceMode);
-        _frisbee.AddTorque(transform.forward*_initialSpeed, _forceMode);
+        _frisbeeRb.AddForce(transform.forward*_initialSpeed, _forceMode);
+        _frisbeeRb.AddTorque(transform.forward*_initialSpeed, _forceMode);
     }
 
     private void Reset()
     {
-        _frisbee.AddForce(Vector3.zero,ForceMode.Impulse);
-        _frisbee.velocity = Vector3.zero;
-        _frisbee.GetComponent<Collider>().isTrigger = true;
-        _frisbee.transform.position = Vector3.zero;
-        _frisbee.transform.rotation = Quaternion.identity;
-        _frisbee.transform.parent = _parent;
+        _frisbeeRb.AddForce(Vector3.zero,ForceMode.Impulse);
+        _frisbeeRb.AddTorque(Vector3.zero, ForceMode.Impulse);
+        _frisbeeRb.velocity = Vector3.zero;
+        _frisbeeRb.GetComponent<Collider>().isTrigger = true;
+        _frisbeeRb.transform.position = Vector3.zero;
+        _frisbeeRb.transform.rotation = Quaternion.identity;
+        _frisbeeRb.transform.parent = _parent;
         _hasLaunched = false;
-
     }
 
-    #region Kinematic Equations
-
-    private float CalculateDistance()
+    private Vector3 GetCurvePoint(float delta, Vector3 startingPos, Vector3 turningPos, Vector3 endingPos)
     {
-        return 0.0f;
+        float inverseDelta = 1 - delta;
+        float deltaSquared = delta*delta;
+        float inverseDeltaSquared = inverseDelta * inverseDelta;
+        return (inverseDeltaSquared * startingPos) + (2 * inverseDelta * delta * turningPos) + (deltaSquared * endingPos);
     }
-
-    private float CalculateTime()
-    {
-        return 0.0f;
-    }
-
-
-
-    #endregion
 }
